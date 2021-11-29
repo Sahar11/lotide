6caf1cd663961b07007d43afc1c2c0cf7ef5e792
Sahar11 <saharhalim151@gmail.com>
2021-11-28 22:29:29 -0500

    findKeys function

---
+++ findKey.js
@@ -0,0 +1,44 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKey = function(object, callback) {
+  for (const key in object){
+    //console.log(object[key]);
+    if(callback(object[key])) {
+      return key;
+    } 
+  }
+};
+
+const key = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2) // => "noma"
+console.log(key);
+
+const key1 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars > 2) // => "Akaleri"
+console.log(key1);
+
+assertEqual(key1, "Akaleri");
+
+const key2 = findKey({
+}, x => x = undefined) // => "undefined"
+console.log(key1);
+
+assertEqual(key2, undefined);
