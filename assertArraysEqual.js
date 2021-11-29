d6d63c5263ddb5a76c31266af9c36d8fdc582d8c
Sahar11 <saharhalim151@gmail.com>
2021-11-28 21:07:06 -0500

    More Refined

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -7,29 +7,29 @@ const assertEqual = function(actual, expected) {
 };
 
 const eqArrays = function(arr1, arr2){
-if (arr1.length !== arr2.length) return false;
-for(let i = 0; i <= arr1.length; i++ ) {
+if (arr1.length !== arr2.length) { 
+  return false;
+} else {
+  for(let i = 0; i <= arr1.length; i++ ) {
   if(arr1[i] !== arr2[i]){
     return false;
+   }
   }
 }
 return true;
 }
 
-const assertArraysEqual = function (arr1, arr2){
-  console.log(arr2);
-if (arr1.length !== arr2.length) return false;
-for(let i = 0; i <= arr1.length; i++ ) {
-  if(arr1[i] !== arr2[i]){
-    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} === ${arr2}`);
-    return false;
-  }
+const assertArraysEqual = function (actual, expected){
+    
+  if(eqArrays(actual, expected)){
+  console.log(`✅✅✅ Assertion passed: ${actual} === ${expected}`);
+  
+} else {
+console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
 }
-console.log(`✅✅✅ Assertion passed: ${arr1} === ${arr2}`);
-return true;
 };
 
 
 
-assertArraysEqual([1, 2, 3], ["a", 2, 3]); // => true
+assertArraysEqual([1, 2, 3], ["a", 2, 3]); // => false
 
