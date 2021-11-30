515a03b8b8edb7138cd2b5af45b53f30246af0ce
Sahar11 <saharhalim151@gmail.com>
2021-11-29 21:42:34 -0500

    shorthandValues

---
+++ Tinker.js
@@ -0,0 +1,28 @@
+////ES6 //shorthand values
+let cat = 'Miaow';
+let dog = 'Woof';
+let bird = 'Peet peet';
+
+let someObject = {
+  cat,
+  dog,
+  bird
+}
+
+console.log(someObject);
+
+//{
+//  cat: "Miaow",
+//  dog: "Woof",
+//  bird: "Peet peet"
+//}
+////////////ES5//////
+var cat = 'Miaow';
+var dog = 'Woof';
+var bird = 'Peet peet';
+
+var someObject = {
+  cat: cat,
+  dog: dog,
+  bird: bird
+}
