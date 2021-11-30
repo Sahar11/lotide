75f0e4fc11afec9a75146a51effdf6f86e56fcae
Sahar11 <saharhalim151@gmail.com>
2021-11-30 10:24:23 -0500

    mochaTest

---
+++ withoutTest.js
@@ -0,0 +1,26 @@
+const assertArraysEqual = require('../assertArraysEqual');
+const without = require('../without');
+const assert = require('chai').assert;
+
+describe("#tail", () => {
+  it('returns ["Yo Yo", "Labs"] of ["Yo Yo", "Lighthouse", "Labs"]', () => {
+    const words = ["Yo Yo", "Lighthouse", "Labs"];
+    assert.strictEqual(assertArraysEqual(without(words, "Lighthouse"), ["Yo Yo", "lighthouse", "Labs"]));
+  });
+  it('returns ["hello", "lighthouse"] of ["hello", "loopy", "lighthouse"]', () => {
+    const words = ["hello", "loopy", "lighthouse"];
+    assert.strictEqual(assertArraysEqual(without(words, "loopy"), ["hello", "loopy", "lighthouse"])); 
+  });
+  it("returns [2, 3] of [1, 2, 3]", () => {
+    const numbers = [1, 2, 3];
+    assert.strictEqual(assertArraysEqual(without([1, 2, 3], [1]), [2, 3])); 
+  });
+});
+
+// console.log(without([1, 2, 3], [1])); // => [2, 3]
+// console.log(without(["1", "2", "3"], [1, 2, "3"])); // => ["1", "2"]
+
+// const words = ["hello", "loopy", "lighthouse"];
+// assertArraysEqual(without(words, "loopy"), ["hello", "loopy", "lighthouse"]);
+
+module.exports = without;
