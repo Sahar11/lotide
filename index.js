0d51b890bec88abccceb3c82df89bf4a12a9771d
Sahar11 <saharhalim151@gmail.com>
2021-11-29 21:46:15 -0500

    refactWithShorthand

---
+++ index.js
@@ -0,0 +1,36 @@
+const head   = require('./head');
+const tail   = require('./tail');
+const middle = require('./middle');
+const eqArrays = require('./eqArrays');
+const assertEqual = require('./assertEqual');
+const assertArraysEqual = require('./assertArraysEqual');
+const findKey = require('./findKey');
+const map = require('./map');
+const takeUntil = require('./takeUntil');
+
+module.exports = {
+  head,
+  tail,
+  middle,
+  eqArrays,
+  assertEqual,
+  assertArraysEqual,
+  findKey,
+  map,
+  takeUntil
+};
+
+//eqArrays: eqArrays,
+//   assertEqual: assertEqual,
+//   assertArraysEqual: assertArraysEqual,
+//   assertObjectsEqual: assertObjectsEqual,
+//   eqObjects: eqObjects,
+//   findKey: findKey,
+//   findKeyByValue: findKeyByValue,
+//   flatten: flatten,
+//   countLetters: countLetters,
+//   countOnly: countOnly,
+//   letterPositions: letterPositions,
+//   map: map,
+//   takeUntil: takeUntil,
+//   without: without
