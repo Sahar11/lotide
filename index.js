d4d7cde23f12af97b41fe2978f80546f1851ea46
Sahar Halim <saharhalim151@gmail.com>
2021-12-01 15:57:14 -0500

    added functions

--- index.js
+++ index.js
@@ -3,10 +3,17 @@ const tail   = require('./tail');
 const middle = require('./middle');
 const eqArrays = require('./eqArrays');
 const assertEqual = require('./assertEqual');
+const assertObjectsEqual = require('./assertObjectsEqual');
 const assertArraysEqual = require('./assertArraysEqual');
+const eqObjects = require('./eqObjects');
+const without = require('./without');
 const findKey = require('./findKey');
 const map = require('./map');
 const takeUntil = require('./takeUntil');
+const countOnly = require('./countOnly');
+const letterPositions = require('./letterPositions');
+const countLetters = require('./countLetters');
+const findKeyByValue = require('./findKeyByValue');
 
 module.exports = {
   head,
@@ -17,20 +24,12 @@ module.exports = {
   assertArraysEqual,
   findKey,
   map,
-  takeUntil
-};
-
-//eqArrays: eqArrays,
-//   assertEqual: assertEqual,
-//   assertArraysEqual: assertArraysEqual,
-//   assertObjectsEqual: assertObjectsEqual,
-//   eqObjects: eqObjects,
-//   findKey: findKey,
-//   findKeyByValue: findKeyByValue,
-//   flatten: flatten,
-//   countLetters: countLetters,
-//   countOnly: countOnly,
-//   letterPositions: letterPositions,
-//   map: map,
-//   takeUntil: takeUntil,
-//   without: without
+  takeUntil,
+  assertObjectsEqual,
+  eqObjects,
+  findKeyByValue,
+  countLetters,
+  countOnly,
+  letterPositions,
+  without
+}
